5efa3d997d00cc39faa2ecab306f6a179f1d78af
Bisrat Kebere <kiyyakebere@gmail.com>
2023-04-15 01:23:43 +0300

    :art: Add cors middleware

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const hotels = require('./routes/hotels');
-const menus = require('./routes/menus');
-const mongoose = require('mongoose');
+const hotels = require("./routes/hotels");
+const menus = require("./routes/menus");
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/MenuHub')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch((err) => console.error('Failed to connect to MongoDB:', err));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+var cors = require("cors");
+
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+mongoose
+  .connect("mongodb://localhost:27017/MenuHub")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Failed to connect to MongoDB:", err));
 
 app.use(express.json());
-app.use('/api/hotels', hotels);
-app.use('/api/menus', menus);
+app.use("/api/hotels", hotels);
+app.use("/api/menus", menus);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`App listening on port ${port}!`));
